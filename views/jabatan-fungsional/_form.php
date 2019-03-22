fcd0d92b0435f009761cb6bf4775dea9162af6e1
piant <piant.grunger@gmail.com>
2019-03-22 09:50:57 +0700

    resik2

--- views/jabatan-fungsional/_form.php
+++ views/jabatan-fungsional/_form.php
@@ -117,52 +117,7 @@ $dataSatuanKerja = ArrayHelper::map(
   </div>
 
 
-   <div class="row">
-        <label class="col-md-3 col-form-label">Eselon</label>
-        <div class="col-md-6">
-
-    <?= $form->field($model, 'id_eselon')->widget(Select2::className(), [
-        'data' => $dataEselon,
-        'options' => ['placeholder' => 'Pilih Eselon...'],
-        'pluginOptions' => [
-            'allowClear' => true,
-        ],
-    ])->label(false); ?>
-</div>
-  </div>
-    <div class="row">
-        <label class="col-md-3 col-form-label"><?= $model->getAttributeLabel('nilai_jabatan') ?></label>
-`        <div class="col-md-6">
-
-
-
-    <?= $form->field($model, 'nilai_jabatan')->textInput(['maxlength' => true])->label(false); ?>
-</div>
-</div>
-
-    <div class="row">
-        <label class="col-md-3 col-form-label"><?= $model->getAttributeLabel('ikkd') ?></label>
-`        <div class="col-md-6">
-
-
-    <?= $form->field($model, 'ikkd')->textInput(['maxlength' => true])->label(false); ?>
-</div>
-</div>
-    <div class="row">
-        <label class="col-md-3 col-form-label"><?= $model->getAttributeLabel('tpp_dinamis') ?></label>
-`        <div class="col-md-6">
-
-
-    <?= $form->field($model, 'tpp_dinamis')->textInput(['maxlength' => true, 'readOnly' => true])->label(false); ?>
-</div>
-</div>
-    <div class="row">
-        <label class="col-md-3 col-form-label"><?= $model->getAttributeLabel('tpp_statis') ?></label>
-`        <div class="col-md-6">
-
-
-    <?= $form->field($model, 'tpp_statis')->textInput(['maxlength' => true])->label(false); ?>
-</div>
+ 
 </div>
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']); ?>
